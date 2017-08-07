a3d3a53c94de764aea7c522002cb5ddfcec8b25b
nhoxbypass@gmail.com <nhoxbypass@gmail.com>
2017-08-07 21:37:38 +0700

    Interacts with sprites

--- core/src/com/iceteaviet/clonybird/ClonyBird.java
+++ core/src/com/iceteaviet/clonybird/ClonyBird.java
@@ -2,14 +2,19 @@ package com.iceteaviet.clonybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ClonyBird extends ApplicationAdapter {
+	private static final int STATE_IDLE = 0;
+	private static final int STATE_PLAYING = 1;
 	SpriteBatch batch;
 	Texture bg;
 	Texture[] bird;
+	private int currGameState = STATE_IDLE;
+	private int currBirdY = 100;
+	private int gravity = 2;
+	private int velocity = 0;
 
 	private int flapState = 0;
 	
@@ -24,11 +29,28 @@ public class ClonyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		if (currGameState == STATE_IDLE) {
+			if (Gdx.input.justTouched()) {
+				currGameState = STATE_PLAYING;
+				currBirdY = Gdx.graphics.getHeight() / 2 - bird[flapState].getHeight() / 2;
+			}
+		} else {
+			//Playing
+			if (Gdx.input.justTouched()) {
+				velocity = 30;
+			}
+
+			if (currBirdY > 0 || velocity > 0) {
+				velocity -= gravity;
+				currBirdY += velocity;
+			}
+		}
+
 		flapState = 1 - flapState;
 
 		batch.begin();
 		batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(bird[flapState], Gdx.graphics.getWidth()/2 - bird[flapState].getWidth()/2, Gdx.graphics.getHeight()/2 - bird[flapState].getWidth()/2);
+		batch.draw(bird[flapState], Gdx.graphics.getWidth() / 2 - bird[flapState].getWidth() / 2, currBirdY);
 		batch.end();
 	}
 	
